6c7602330807bca9773035f98163027a5c5f55da
joojask <jaskjo19@hotmail.fi>
2020-09-13 17:34:01 +0300

    Tehtavan korjailua

--- Tehtava3.java
+++ Tehtava3.java
@@ -6,18 +6,17 @@
 public class Tehtava3 {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-
-        double nettoHinta = 0.0, bruttoHinta = 0.0, alv = 0.0;
-
-        System.out.println("Tervehdys tasamaantallaaja");
-        System.out.print("Syöttäisitkö tuotteen nettohinnan\n> ");
-        nettoHinta = Double.valueOf(lukija.nextLine());
-
-        System.out.print("Syöttäisitkö alv-prosentin\n> ");
-
-        alv = Double.valueOf(lukija.nextLine());
-        bruttoHinta = (nettoHinta + ((alv / 100) * nettoHinta));
-        System.out.println("Bruttohinta on: " + bruttoHinta + " €");
+        // Muuttujien alustus
+        double muuttuja1 = 0.0, muuttuja2 = 0.0, muuttuja3 = 0.0;
+        // Ohjelma alkaa
+        System.out.println("Tervehdys");
+        System.out.print("Syöttäisitkö muuttujan yksi\n> ");
+        // Muuttujien
+        muuttuja1 = Double.valueOf(lukija.nextLine());
+        System.out.print("Syöttäisitkö muuttujan kolme\n> ");
+        muuttuja3 = Double.valueOf(lukija.nextLine());
+        muuttuja2 = (muuttuja1 + ((muuttuja3 / 100) * muuttuja1));
+        System.out.println("muuttuja2 on: " + muuttuja2 + " yksikkoa");
         lukija.close();
     }
 }
