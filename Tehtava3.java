618325502b78206903cd1d35eb5ea4fd99c0bf13
joojask <jaskjo19@hotmail.fi>
2020-09-13 14:50:58 +0300

    Ensimmainen

---
+++ Tehtava3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/*3. Tee yrityksen taloushallinnon järjestelmään ohjelma, 
+*joka kysyy käyttäjältä kustannuksen nettohinnan ja alv-prosentin.
+*Tietojen perusteella ohjelma laskee ja tulostaa kustannuksen bruttohinnan.
+@author (C)JoonasJaskari 13.9.2020
+*/
+
+public class Tehtava3 {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        double nettoHinta = 0.0, bruttoHinta = 0.0, alv = 0.0;
+
+        System.out.println("Tervehdys tasamaantallaaja");
+        System.out.print("Syöttäisitkö tuotteen nettohinnan\n> ");
+        nettoHinta = Double.valueOf(lukija.nextLine());
+        System.out.print("Syöttäisitkö alv-prosentin\n> ");
+        alv = Double.valueOf(lukija.nextLine());
+        bruttoHinta = (nettoHinta + ((alv / 100) * nettoHinta));
+        System.out.println("Bruttohinta on: " + bruttoHinta + " €");
+        lukija.close();
+    }
+}
